17043aa53e52ffb65940f9b23d07beeacb7b42c5
Компьютер дом <denismikhno@gmail.com>
2022-07-11 20:26:34 +0300

    Home_11.07.22_20:30

--- Saluton/src/com/java24hourse/Soluton.java
+++ Saluton/src/com/java24hourse/Soluton.java
@@ -3,7 +3,7 @@ package com.java24hourse;
 public class Soluton {
 
 	public static void main(String[] arguments) {
-		String greeting = "Soluton mondo!";
+		String greeting = "\033[31mПривет \033[32mиз \033[33mмира \033[34mJava!";
 		System.out.println(greeting);
 
 	}
