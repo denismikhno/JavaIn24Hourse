9b32cc4e1a78e1e813eb84ebfdcfc5a89f38340a
Компьютер работа <denismikhno@gmail.com>
2022-07-11 10:16:27 +0300

    Первый проект из книги

---
+++ Saluton/src/com/java24hourse/Soluton.java
@@ -0,0 +1,11 @@
+package com.java24hourse;
+
+public class Soluton {
+
+	public static void main(String[] arguments) {
+		String greeting = "Soluton mondo!";
+		System.out.println(greeting);
+
+	}
+
+}
